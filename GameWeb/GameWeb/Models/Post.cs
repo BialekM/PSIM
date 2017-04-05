e03cf98a2cd2765086a7527d26e3ae2e31ba08f4
bialas95 <michalbialek1995@outlook.com>
2017-04-05 16:03:08 +0200

    entity-baza
    baza

---
+++ GameWeb/GameWeb/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GameWeb.Models
+{
+    public class Post
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public User User { get; set; }
+        public Game Game { get; set; }
+        public string Text { get; set; }
+        public DateTime PostDate { get; set; }
+
+    }
+}
